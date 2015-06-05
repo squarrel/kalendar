95a9c510d498825cbec55ece1f7c84f5d331c7e3
Milos <milosbulatovic3@gmail.com>
2015-06-05 07:15:11 +0200

    using an environment variable for a value in settings.py

--- kalendar/settings.py
+++ kalendar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zjb#xp@n^6&_b0&-fg1$pc1u71d46mpu=&f400it7uwd3+tce2'
+SECRET_KEY = $DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
